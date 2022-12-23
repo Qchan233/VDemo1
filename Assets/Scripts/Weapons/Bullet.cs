54ccff683778f2f85b1fbe5d5999d92437316c16
於琦 <Moeyfishyq@outlook.com>
2022-12-23 13:56:45 -0800

    Weapons Working

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,8 @@ public class Bullet : MonoBehaviour
 {
     private Vector3 _direction;
     [SerializeField] private float _speed = 10;
-    
+    private const int NUMFIND = 3;
+    [SerializeField] private float _bulletRange = 10;
     public float Speed
     {
         set => _speed = value;
@@ -17,8 +19,28 @@ public class Bullet : MonoBehaviour
         set => _direction = value;
     }
 
+    private void Start()
+    {
+        _direction = AimAtEnemy();
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, _direction); 
+    }
+
     void FixedUpdate()
     {
         transform.position += _direction * _speed;
+    }
+    private Vector3 AimAtEnemy()
+    {
+        var results = new Collider2D[NUMFIND];
+        Physics2D.OverlapCircleNonAlloc(transform.position, _bulletRange, results);
+
+        foreach (var result in results)
+        {
+            if (result!= null && result.CompareTag("Enemy"))
+            {
+                return (result.transform.position - transform.position).normalized;
+            }
+        }
+        return new Vector3(1,0,0);
     }
 }
--- Assets/Scripts/Weapons/WeaponFire.cs
+++ Assets/Scripts/Weapons/WeaponFire.cs
@@ -10,11 +10,12 @@ public class WeaponFire : MonoBehaviour
 {
     // Start is called before the first frame update
     
-    [SerializeField] private float _coolDown = 0.6f;
-    [SerializeField] private float _weaponRange = 10;
+    [SerializeField] private float _coolDown;
+
     [SerializeField] private bool fireEnabled = true;
     [SerializeField] private GameObject bullet;
-    private const int NUMFIND = 3;
+    [SerializeField] private UnityEvent FireEvent;
+
     private Vector3 _direction;
     
     private void Start()
@@ -24,28 +25,7 @@ public class WeaponFire : MonoBehaviour
 
     public void FireBullet()
     {
-        _direction = AimAtEnemy();
-        if (_direction == Vector3.zero)
-        {
-            _direction = new Vector3(1, 0, 0);
-        }
-
-        Instantiate(bullet, transform.position, Quaternion.LookRotation(Vector3.forward, _direction));
-        bullet
-    }
-    
-    private Vector3 AimAtEnemy()
-    {
-        var results = new Collider2D[NUMFIND];
-        Physics2D.OverlapCircleNonAlloc(transform.position, _weaponRange, results);
-
-        foreach (var result in results)
-        {
-            if (result!= null && result.CompareTag("Enemy"))
-            {
-                return (result.transform.position - transform.position).normalized;
-            }
-        }
-        return new Vector3(1,0,0);
+        Instantiate(bullet, transform.position, Quaternion.identity);
+        FireEvent.Invoke();
     }
 }
